22738aa878c68593fe5e1411cb102462735a3e6b
johnscillieri <john.scillieri@visionistinc.com>
2016-12-03 14:55:08 -0500

    Add dockerfile, readme and run script.

--- johnscillieri/03/main.nim
+++ johnscillieri/03/main.nim
@@ -7,7 +7,7 @@ template check_triangle( sides: openarray[int] ): bool =
 let data_array = mapIt( toSeq( lines("input") ), mapIt( it.splitWhitespace(), it.parseInt ) )
 
 # Part 1
-echo len( filterIt( data_array, check_triangle( it ) ) )
+echo "Part 1 Solution: ", len( filterIt( data_array, check_triangle( it ) ) )
 
 # Part 2
 var count = 0
@@ -15,4 +15,4 @@ for i in countup(2, len(data_array), 3):
     if check_triangle( [data_array[i][0], data_array[i-1][0], data_array[i-2][0]] ): count += 1
     if check_triangle( [data_array[i][1], data_array[i-1][1], data_array[i-2][1]] ): count += 1
     if check_triangle( [data_array[i][2], data_array[i-1][2], data_array[i-2][2]] ): count += 1
-echo count
+echo "Part 2 Solution: ", count
