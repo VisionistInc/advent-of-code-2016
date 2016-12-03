85322bf6f0acf20fd75361e075cf1c6d931c379d
johnscillieri <john.scillieri@visionistinc.com>
2016-12-03 14:19:11 -0500

    Speedup loop

--- johnscillieri/03/main.nim
+++ johnscillieri/03/main.nim
@@ -11,9 +11,8 @@ echo len( filterIt( data_array, check_triangle( it ) ) )
 
 # Part 2
 var count = 0
-for i in 0 .. <len(data_array):
-    if i mod 3 == 2:
-        if check_triangle( [data_array[i][0], data_array[i-1][0], data_array[i-2][0]] ): count += 1
-        if check_triangle( [data_array[i][1], data_array[i-1][1], data_array[i-2][1]] ): count += 1
-        if check_triangle( [data_array[i][2], data_array[i-1][2], data_array[i-2][2]] ): count += 1
+for i in countup(2, len(data_array), 3):
+    if check_triangle( [data_array[i][0], data_array[i-1][0], data_array[i-2][0]] ): count += 1
+    if check_triangle( [data_array[i][1], data_array[i-1][1], data_array[i-2][1]] ): count += 1
+    if check_triangle( [data_array[i][2], data_array[i-1][2], data_array[i-2][2]] ): count += 1
 echo count
