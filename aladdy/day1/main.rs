9ac960b033f6f29d9e87d67b21917017dd39a391
Alex Addy <alex.addy@gmail.com>
2016-12-04 09:37:26 -0500

    Solution for day 1 now does both parts

--- aladdy/day1/main.rs
+++ aladdy/day1/main.rs
@@ -1,11 +1,12 @@
 use std::io;
+use std::collections::HashSet;
 
 fn main() {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
             let v: Vec<&str> = input.trim().split(", ").collect();
-            println!("{}", solve(&v));
+            println!("Ending is {} blocks away", solve(&v));
         },
         Err(error) => println!("error: {}", error),
     }
@@ -25,9 +26,11 @@ fn solve(v: &Vec<&str>) -> isize {
     let mut x: isize = 0;
     let mut y: isize = 0;
     let mut dir = 0; // start facing north
+    let mut visited = HashSet::new();
+    let mut first = false;
     
     for step in v {
-        let (direction, distance) = get_travel(step);
+        let (direction, mut distance) = get_travel(step);
         match direction {
             Direction::Right => {
                 dir = (dir + 1) % 4; // turn right, modulus for full rotation
@@ -41,22 +44,31 @@ fn solve(v: &Vec<&str>) -> isize {
             }
         }
         
-        match dir {
-            N => {
-                y = y + distance
-            },
-            E => {
-                x = x + distance
-            },
-            S => {
-                y = y - distance
-            },
-            W => {
-                x = x - distance
-            },
-            _ => unreachable!(),
+        while distance > 0 {
+            match dir {
+                N => {
+                    y = y + 1
+                },
+                E => {
+                    x = x + 1
+                },
+                S => {
+                    y = y - 1
+                },
+                W => {
+                    x = x - 1
+                },
+                _ => unreachable!(),
+            }
+            distance -= 1;
+
+            if first == false && visited.contains(&(x,y)) {
+                println!("First doubly visited location is {:?} which is {} blocks away",
+                    (x,y), x.abs()+y.abs());
+                first = true;
+            }
+            visited.insert((x,y));
         }
-        println!("{} {} {} {}", dir, distance, x, y);
     }
     
     x.abs() + y.abs()
