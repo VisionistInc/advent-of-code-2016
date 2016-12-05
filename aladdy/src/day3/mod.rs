f78ab9f566327109f56f8a0ecd125efcd9c238b8
Alex Addy <alex.addy@visionistinc.com>
2016-12-04 21:45:11 -0500

    Solution to day 3 part 2

--- aladdy/src/day3/mod.rs
+++ aladdy/src/day3/mod.rs
@@ -1,6 +1,8 @@
 use std::io;
 
 pub fn solve<L: Iterator<Item=io::Result<String>>>(input: &mut L) -> () {
+    let mut entries: Vec<(usize, usize, usize)> = vec![];
+
     let mut possible = 0;
     for line in input {
         let mut sides: Vec<usize> = line.unwrap()
@@ -9,10 +11,35 @@ pub fn solve<L: Iterator<Item=io::Result<String>>>(input: &mut L) -> () {
                 usize::from_str_radix(s, 10).unwrap()
             })
             .collect();
+           
+        // has to happen before sort reorders items
+        entries.push((sides[0], sides[1], sides[2]));
+
         sides.sort();
         if sides[0] + sides[1] > sides[2] {
             possible += 1;
         }
     }
-    println!("{}", possible);
+    println!("{} triangles possible horizontally", possible);
+
+    possible = 0;
+    for chunk in entries.chunks(3) {
+        let triangles = rotate(chunk);
+        possible += triangles.into_iter()
+            .filter(is_valid_tri)
+            .count();
+    }
+    println!("{} triangles possible vertically", possible);
+}
+
+fn rotate(v: &[(usize, usize, usize)]) -> Vec<(usize, usize, usize)> {
+    vec![(v[0].0, v[1].0, v[2].0),
+         (v[0].1, v[1].1, v[2].1),
+         (v[0].2, v[1].2, v[2].2)]
+}
+
+fn is_valid_tri(t: &(usize, usize, usize)) -> bool {
+    (t.0 + t.1 > t.2) &
+    (t.1 + t.2 > t.0) &
+    (t.2 + t.0 > t.1)
 }
