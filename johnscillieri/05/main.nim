14f7d7357515556c20a212cd3f693179c99c704c
johnscillieri <john.scillieri@visionistinc.com>
2016-12-06 21:25:45 -0500

    Add day 5 solution and animated gif for movie effect

--- johnscillieri/05/main.nim
+++ johnscillieri/05/main.nim
@@ -3,32 +3,38 @@ import md5
 import random
 import sequtils
 import strutils
+import os
 
-proc sixth_character( s: string ): string =
+proc position_and_character( s: string ): tuple[position: int, character: char] =
     # Return the valid character else an empty string
     let hash = getMD5( s )
-    if hash.startswith("00000"): $hash[5] else: ""
+    if hash.startswith("00000") and hash[5] in {'0'..'7'}:
+        result = (parseInt($hash[5]), hash[6])
+    else:
+        result = (-1, 'x')
 
 template random_letter(): string =
     $random( "abcdefghijklmnopqrstuvwxyz" )
 
-template random_string( length: int ): string =
-    join( lc[ random_letter() | ( _ <- 0..<length ), string ] )
+proc movie_string[T]( letters: openarray[T] ): string =
+    result = ""
+    for letter in items( letters ):
+        result &= (if letter == '\0': random_letter() else: $letter)
 
 proc main() =
     let base = "ffykfhsq"
-    var result = ""
+    var result = newString(8)
 
     var index = 0
-    while result.len < 8:
-        let character = sixth_character( base & $index )
+    while '\0' in result:
+        let (position, character) = position_and_character( base & $index )
         index += 1
-        if character != "":
-            result &= character
+        if position != -1 and result[position] == '\0':
+            result[position] = character
         if index mod 100000 == 0:
-            stdout.write( "Password: " & result & random_string( 8 - len(result) ) & "\r" )
+            stdout.write( "Cracking password: " & movie_string(result) & "\r" )
             stdout.flushFile
-    echo( "Password: " & result )
+    echo( "Cracking password: " & result )
     echo "\nDone."
 
 when isMainModule:
