9f60ffb0db79a4bdb2cb398e4f392c24bbe26b64
rawjoe <github@ballsandshaft.net>
2016-12-08 00:06:26 -0500

    Fixing bug in day 5 part 2.

--- rawjoe/05/sol2.sh
+++ rawjoe/05/sol2.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-INPUT=ojvtpuvg
+if [ $# -eq 1 ]; then
+    INPUT=$1
+else
+    INPUT=ojvtpuvg
+    echo Used default input
+fi
 
-COUNTER=0
+echo Using $INPUT as input
+
 INDEX=0
 
-ARRAY=(0 0 0 0 0 0 0 0)
+ARRAY=(z z z z z z z z)
 
-until [ $COUNTER -eq 8 ]; do
+until ! [[ ${ARRAY[*]} == *"z"* ]]; do
     STR=$(echo -n $INPUT$INDEX | md5sum)
     if [[ $STR == 00000* ]]; then
-        let i=${STR:5:1}
-        if [[ $i -lt ${#ARRAY[@]} ]]; then
-            if [[ ${ARRAY[$i]} == 0 ]]; then
+        i=${STR:5:1}
+        if [[ $i < ${#ARRAY[@]} ]]; then
+            if [[ ${ARRAY[$i]} == "z" ]]; then
                 echo $INDEX gives $STR
                 ARRAY[$i]=${STR:6:1}
-                let COUNTER+=1
                 echo ${ARRAY[@]}
             fi
         fi
