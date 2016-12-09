2be6fe50aa051a5957632d029849c97f49b688cf
Trey Hoover <treyhoover@gmail.com>
2016-12-09 00:01:26 -0500

    Refactor answer 2 for part 2

--- treyhoover/02/index.js
+++ treyhoover/02/index.js
@@ -1,42 +1,48 @@
 const fs = require('fs');
 const input = fs.readFileSync('index.txt', 'utf-8');
 
+// const numPad = [
+//   ['1', '2', '3'],
+//   ['4', '5', '6'],
+//   ['7', '8', '9']
+// ];
+
 const numPad = [
-  ['1', '2', '3'],
-  ['4', '5', '6'],
-  ['7', '8', '9']
+  [' ', ' ', '1', ' ', ' '],
+  [' ', '2', '3', '4', ' '],
+  ['5', '6', '7', '8', '9'],
+  [' ', 'A', 'B', 'C', ' '],
+  [' ', ' ', 'D', ' ', ' ']
 ];
 
 const position = { x: 1, y: 1 };
 const lines = input.split('\n').map(line => line.split(''));
 
+const isBlank = cell => typeof cell !== 'string' || !cell.trim();
+const isValid = (x, y) => Array.isArray(numPad[y]) && !isBlank(numPad[y][x]);
+
 const move = {
-  up() {
-    if (position.y > 0) position.y--;
+  U() {
+    if (isValid(position.x, position.y - 1)) position.y--;
   },
 
-  right() {
-    if (position.x < 2) position.x++;
+  R() {
+    if (isValid(position.x + 1, position.y)) position.x++;
   },
 
-  down() {
-    if (position.y < 2) position.y++;
+  D() {
+    if (isValid(position.x, position.y + 1)) position.y++;
   },
 
-  left() {
-    if (position.x > 0) position.x--;
+  L() {
+    if (isValid(position.x - 1, position.y)) position.x--;
   }
 };
 
 const getCurrentNumber = () => numPad[position.y][position.x];
 
 const answer = lines.reduce((number, line) => {
-  line.forEach(d => {
-    if (d === 'U') move.up();
-    if (d === 'R') move.right();
-    if (d === 'D') move.down();
-    if (d === 'L') move.left();
-  });
+  line.forEach(d => move[d] && move[d]());
 
   return number + getCurrentNumber();
 }, '');
