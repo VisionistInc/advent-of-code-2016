79940060ce38054c416b90c919e3502eaf0d4389
Trey Hoover <treyhoover@gmail.com>
2016-12-20 23:14:41 -0500

    So close...

--- treyhoover/09/partTwo.js
+++ treyhoover/09/partTwo.js
@@ -2,30 +2,27 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf-8');
 const lines = input.split('\n');
 
-const unzip = (str, callback) => {
-  console.log(str.length);
-
+const unzip = (str, multiplier = 1) => {
   const start = str.indexOf('(');
   const stop = str.indexOf(')');
+
+  if (start <= -1 || stop <= start) return str.repeat(multiplier);
+
   const selector = str.slice(start + 1, stop).split('x');
   const count = parseInt(selector[0]);
   const repeat = parseInt(selector[1]);
 
-  if (start > -1 && stop > start) {
-    const beginning = str.slice(0, start);
-    const repeated = str.slice(stop + 1, stop + count + 1).repeat(repeat);
-    const end = str.slice(stop + count + 1, str.length);
+  const prev = str.slice(0, start, 1);
+  const next = str.slice(stop + 1, stop + count + 1);
+  const remainder = str.slice(stop + count + 1, str.length);
 
-    setTimeout(() => {
-      unzip(beginning + repeated + end, callback);
-    }, 0)
-  } else {
-    callback(str)
-  }
+  const compounded = unzip(next, multiplier * repeat);
+  const carriedForward = unzip(remainder, multiplier);
+
+  return prev + compounded + carriedForward;
 };
 
 lines.forEach(line => {
-  unzip(line, text => {
-    console.log(text.length);
-  });
+  const unzipped = unzip(line);
+  console.log('unzipped', unzipped.length);
 });
