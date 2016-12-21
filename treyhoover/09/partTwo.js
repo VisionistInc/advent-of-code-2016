b736c4e2de9547ef7b09f506d8dc2cf4176d504f
Trey Hoover <treyhoover@gmail.com>
2016-12-20 23:59:53 -0500

    Refactor to simply use length instead of actually decompressing the file...

--- treyhoover/09/partTwo.js
+++ treyhoover/09/partTwo.js
@@ -6,23 +6,23 @@ const unzip = (str, multiplier = 1) => {
   const start = str.indexOf('(');
   const stop = str.indexOf(')');
 
-  if (start <= -1 || stop <= start) return str.repeat(multiplier);
+  if (start <= -1 || stop <= start) return str.length * multiplier;
 
   const selector = str.slice(start + 1, stop).split('x');
   const count = parseInt(selector[0]);
   const repeat = parseInt(selector[1]);
 
-  const prev = str.slice(0, start, 1);
+  const prev = str.slice(0, start);
   const next = str.slice(stop + 1, stop + count + 1);
   const remainder = str.slice(stop + count + 1, str.length);
 
   const compounded = unzip(next, multiplier * repeat);
   const carriedForward = unzip(remainder, multiplier);
 
-  return prev + compounded + carriedForward;
+  return prev.length + compounded + carriedForward;
 };
 
 lines.forEach(line => {
   const unzipped = unzip(line);
-  console.log('unzipped', unzipped.length);
+  console.log('unzipped', unzipped);
 });
